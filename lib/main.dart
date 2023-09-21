829ff7eac1f29090de40782eb35e56cbb606cb75
박종유 <jypark@nextree.io>
2023-09-21 17:42:24 +0900

    feat: Column, Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/screen/column_row.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: ColumnRow(),
   ));
 }
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF99231),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/logo.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
-  }
-}
+// Splash Screen
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFFF99231),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Image.asset(
+//             'asset/img/logo.png',
+//           ),
+//           CircularProgressIndicator(
+//             color: Colors.white,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }

d3397e98178884cc8bb4041e920c0ba2ead05107
박종유 <jypark@nextree.io>
2023-09-22 17:56:33 +0900

    feat: 만난지 몇 일 Project 테마 적용하기
    MeterialApp 에 theme 속성을 추가해서 theme 를 사용할 수 있다.
    
    theme의 이름은 custom 으로 정할 수 있고,
    위젯에서 Theme.of(context); 로 theme 를 가져와
    해당 theme 를 적용시킬 수 있다.

--- lib/screen/meet_date.dart
+++ lib/screen/meet_date.dart
@@ -76,6 +76,8 @@ class _TopPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
     final DateTime now = DateTime.now();
 
     return Expanded(
@@ -84,29 +86,17 @@ class _TopPart extends StatelessWidget {
         children: [
           Text(
             'U&I',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'parisienne',
-              fontSize: 80.0,
-            ),
+            style: textTheme.headlineLarge,
           ),
           Column(
             children: [
               Text(
                 '우리 처음 만난날',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sonflower',
-                  fontSize: 30.0,
-                ),
+                style: textTheme.bodyMedium,
               ),
               Text(
                 '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sonflower',
-                  fontSize: 20.0,
-                ),
+                style: textTheme.bodySmall,
               )
             ],
           ),
@@ -122,12 +112,7 @@ class _TopPart extends StatelessWidget {
                   now.month,
                   now.day,
                 ).difference(selectedDate).inDays + 1}',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'sonflower',
-              fontSize: 50.0,
-              fontWeight: FontWeight.w700,
-            ),
+            style: textTheme.headlineMedium,
           ),
         ],
       ),

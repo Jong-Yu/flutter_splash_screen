f6ac2a5c3aa2958e76dcbdd4ea723d389d6f53e1
박종유 <jypark@nextree.io>
2023-09-22 16:58:01 +0900

    feat: 만난지 몇 일 Project font 설정하기

---
+++ lib/screen/date.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MeetDates extends StatefulWidget {
+  const MeetDates({super.key});
+
+  @override
+  State<MeetDates> createState() => _MeetDatesState();
+}
+
+class _MeetDatesState extends State<MeetDates> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

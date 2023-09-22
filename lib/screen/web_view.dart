4f3f8a3555ce5f1b0394fe372c4e2ad6c8a70630
박종유 <jypark@nextree.io>
2023-09-22 13:57:47 +0900

    feat: Flutter in WebView

---
+++ lib/screen/web_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+// final homeUrl = Uri.parse('https://blog.codefactory.ai');
+final homeUrl = Uri.parse('https://stg.mysuni.sk.com/suni-mobile');
+
+class WebViewScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  WebViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('Code Factory'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  controller.loadRequest(homeUrl);
+                },
+                icon: Icon(Icons.home)),
+          ]),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}

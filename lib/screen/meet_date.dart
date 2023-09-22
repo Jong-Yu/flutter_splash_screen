3cf726029fd95e17936edcfbbea1da4c6643c0da
박종유 <jypark@nextree.io>
2023-09-22 17:34:03 +0900

    feat: 만난지 몇 일 Project
    DatePicker 기능 넣기
    
    maximunDate 를 넣어주면 최대로 선택할 수 있는 날짜를 정할 수 있는데, 이 때 initialDateTime 의 값을 넣어줘야 한다.
    initialDateTime의 값은 maximunDate의 값보다 이 후의 값을 넣어주면 에러를 발생시킨다.

--- lib/screen/meet_date.dart
+++ lib/screen/meet_date.dart
@@ -27,11 +27,24 @@ class _MeetDatesState extends State<MeetDates> {
   }
 }
 
-class _TopPart extends StatelessWidget {
+class _TopPart extends StatefulWidget {
   const _TopPart({super.key});
 
   @override
+  State<_TopPart> createState() => _TopPartState();
+}
+
+class _TopPartState extends State<_TopPart> {
+  DateTime selectedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
   Widget build(BuildContext context) {
+    final DateTime now = DateTime.now();
+
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -55,7 +68,7 @@ class _TopPart extends StatelessWidget {
                 ),
               ),
               Text(
-                '2023.09.22',
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
                 style: TextStyle(
                   color: Colors.white,
                   fontFamily: 'sonflower',
@@ -78,8 +91,16 @@ class _TopPart extends StatelessWidget {
                         height: 300.0,
                         child: CupertinoDatePicker(
                           mode: CupertinoDatePickerMode.date,
+                          initialDateTime: selectedDate,
+                          maximumDate: DateTime(
+                            now.year,
+                            now.month,
+                            now.day,
+                          ),
                           onDateTimeChanged: (DateTime date) {
-                            print(date);
+                            setState(() {
+                              selectedDate = date;
+                            });
                           },
                         ),
                       ),
@@ -91,7 +112,11 @@ class _TopPart extends StatelessWidget {
             iconSize: 60.0,
           ),
           Text(
-            'D+1',
+            'D+${DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ).difference(selectedDate).inDays + 1}',
             style: TextStyle(
               color: Colors.white,
               fontFamily: 'sonflower',

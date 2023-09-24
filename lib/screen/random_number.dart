721e789ddc087f9b6e2757799f84fe7907807de1
박종유 <jypark@nextree.io>
2023-09-24 22:15:52 +0900

    feat: Random Number
    1. 화면이동
      a. Navigator.of(context).push() :: 이동
      b. 화면이동 :: MaterinalPageRoute(build: (BuildContext context){ return Widget(); }
      c. Navigator.of(context).pop() :: 이전화면으로 이동
    2. 화면간 이동할 떄 값 넘겨주고 받기
      a. 값 넘겨주기 :: Navigator.of(context).pop(maxNumber.toInt());
      b. 값 넘겨받기 :: async await 으로 Navigator 함수에서 return 받기
        ex) final int? result = await Navigator.of(context).push<int>(...)
    3. 공통 widget 만들기
      a. 비슷한 기능을 하는 Widget 들은 component 폴더에 만들어 놓고,
         import 해서 사용할 수 있다.

---
+++ lib/screen/random_number.dart
@@ -0,0 +1,152 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:hello_world/component/number_row.dart';
+import 'package:hello_world/constant/color.dart';
+import 'package:hello_world/screen/settings_screen.dart';
+
+class RandomNumber extends StatefulWidget {
+  const RandomNumber({super.key});
+
+  @override
+  State<RandomNumber> createState() => _RandomNumberState();
+}
+
+class _RandomNumberState extends State<RandomNumber> {
+  int maxNumber = 1000;
+  List<int> randomNumbers = [
+    123,
+    456,
+    789,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Header(
+                onPressed: onSettingMaxNumber,
+              ),
+              _Body(randomNumbers: randomNumbers),
+              _Footer(
+                onPressed: onPressed,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  onPressed() {
+    final rand = Random();
+
+    final Set<int> nextNumbers = {};
+
+    while (nextNumbers.length != 3) {
+      final number = rand.nextInt(maxNumber);
+
+      nextNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = nextNumbers.toList();
+    });
+  }
+
+  void onSettingMaxNumber() async {
+    final int? result = await Navigator.of(context).push<int>(
+      MaterialPageRoute(builder: (BuildContext constext) {
+        return SettingsScreen(
+          maxNumber: maxNumber,
+        );
+      }),
+    );
+
+    if (result != null) {
+      setState(() {
+        maxNumber = result;
+      });
+    }
+  }
+}
+
+class _Header extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Header({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '램덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Icons.settings,
+            color: RED_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+  const _Body({required this.randomNumbers, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: randomNumbers
+            .asMap()
+            .entries
+            .map(
+              (x) => Padding(
+                padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                child: NumberRow(number: x.value),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Footer({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text('생성하기!'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: RED_COLOR,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/settings_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/component/number_row.dart';
+
+import '../constant/color.dart';
+
+class SettingsScreen extends StatefulWidget {
+  final int maxNumber;
+  const SettingsScreen({required this.maxNumber, super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 1000;
+
+  @override
+  void initState() {
+    super.initState();
+    maxNumber = widget.maxNumber.toDouble();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              _Body(
+                maxNumber: maxNumber,
+              ),
+              _Footer(
+                  maxNumber: maxNumber,
+                  onChange: onChange,
+                  onPressed: onPressed)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onChange(double value) {
+    setState(() {
+      maxNumber = value;
+    });
+  }
+
+  void onPressed() {
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+  const _Body({required this.maxNumber, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: NumberRow(
+      number: maxNumber.toInt(),
+    ));
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final ValueChanged<double> onChange;
+  final VoidCallback onPressed;
+
+  const _Footer(
+      {required this.maxNumber,
+      required this.onChange,
+      required this.onPressed,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onChange,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+            child: Text('저장'),
+          ),
+        )
+      ],
+    );
+  }
+}

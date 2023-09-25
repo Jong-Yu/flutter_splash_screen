762a8fb2e69ed722c4f3d4f8d472dba0e9eae59b
박종유 <jypark@nextree.io>
2023-09-25 11:30:23 +0900

    feat: Navigate Push
    ### Push, PushNamed
    다음 화면으로 이동
    1. push :: MaterialPageRoute 를 사용하여 builder 로 화면 build
    2. pushNamed :: url을 통해 화면 이동
    
    ### PushReplacement, PushReplacementNamed
    다음 화면으로 이동하지만, 이전화면을 대체한다.
    1. pushReplacement :: MaterialPageRoute
    2. pushReplacementNamed :: url을 통해 화면이동
    
    ### PushAndRemoveUntil, PushNamedAndRemoveUntil
    화면을 이동한 후에, 2번 쨰 parameter 의 값에 따라
    이전 화면 경로를 제거한다.
    true :: 모두살림
    false :: 모두제거
    (){} :: 조건의 맞는 경우에만 살림
    1. pushAndRemoveUntil :: MaterialPageRoute
    ex)
    Navigator.of(context).pushAndRemoveUntil(
      MaterialPageRoute(
        builder: (_) => RouteThreeScreen(),
      ),
      (route) => route.settings.name == '/',
    );
    
    2. pushNamedAndRemoveUntil :: url을 통해 화면이동
    ex)
    Navigator.of(context).pushNamedAndRemoveUntil(
      '/three',
      (route) => route.settings.name == '/',
    );

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,14 @@ import 'package:hello_world/screen/navigation/route_two_screen.dart';
 void main() {
   runApp(
     MaterialApp(
-        // home: NavigationWidget(),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => NavigationWidget(),
-          '/one': (context) => RouteOneScreen(),
-          '/two': (context) => RouteTwoScreen(),
-          '/three': (context) => RouteThreeScreen(),
-        }),
+      // home: NavigationWidget(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => NavigationWidget(),
+        '/one': (context) => RouteOneScreen(),
+        '/two': (context) => RouteTwoScreen(),
+        '/three': (context) => RouteThreeScreen(),
+      },
+    ),
   );
 }
--- lib/screen/navigation/route_two_screen.dart
+++ lib/screen/navigation/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/layout/main_layout.dart';
+import 'package:hello_world/screen/navigation/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
@@ -26,7 +27,50 @@ class RouteTwoScreen extends StatelessWidget {
             Navigator.of(context).pushNamed('/three', arguments: 999);
           },
           child: Text("Push Named"),
-        )
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // push -> replace
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+            );
+          },
+          child: Text("Push ReplaceMent"),
+        ),
+        // pushNamed -> replace Named
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/three');
+          },
+          child: Text("Push Replacement Named"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+              // 해당 조건에 맞는 url 은 살리고 나머지는 제거한다.
+              // false :: 모두 지움
+              // true :: 모두 살림
+              // ex) route.settings.name == '/' :: url 이 '/' 인 경우를 제외한
+              //     나머지는 다 제거한다.
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text("Push And Remove Until"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/three',
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text("PushNamed And Remove Until"),
+        ),
       ],
     );
   }
